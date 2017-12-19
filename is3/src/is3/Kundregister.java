7419ddb56e47978a189ba174fcb277cb64d9fb69
Anton <anton.hejman@gmail.com>
2017-12-19 10:51:15 +0100

    19/12 10:50

--- is3/src/is3/Kundregister.java
+++ is3/src/is3/Kundregister.java
@@ -1,5 +1,32 @@
 package is3;
 
+import java.util.HashMap;
+
 public class Kundregister {
+	
+	
+	private HashMap<String, Kund> kundMap = new HashMap<String, Kund>();
+	
+	public void setKundlista (HashMap<String, Kund> kundMap) {
+		this.kundMap = kundMap;
+	}
+	
+	public HashMap<String, Kund> getKundlista () {
+		return this.kundMap;
+	}
+	
+	public void addKund (Kund k) {
+		this.kundMap.put(k.getKundnummer(), k);
+	}
+	
+	public void removeKund (String kundnummer) {
+		kundMap.entrySet().removeIf(entry -> !kundMap.containsKey(kundnummer));
+		
+	}
+	
+	public HashMap<String, Kund> findKund (String kundnummer) {
+		kundMap.get(kundnummer);
+	
+	}
 
 }
