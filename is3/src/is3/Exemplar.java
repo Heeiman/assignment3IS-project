35776fcc746fddb5be0dbea8db1b5b50fba400f0
Emma <Emma@Emmas>
2017-12-19 10:49:14 +0100

    c2

--- is3/src/is3/Exemplar.java
+++ is3/src/is3/Exemplar.java
@@ -1,5 +1,12 @@
 package is3;
 
 public class Exemplar {
-
+	private String serienummer;
+	
+	public void setSerienummer (String serienummer) {
+		this.serienummer = serienummer;
+	}
+	public String getSerienummer () {
+		return serienummer;
+	}
 }
