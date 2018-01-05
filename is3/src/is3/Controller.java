ff8e5a7aea17cff31cf9ddf2957ae86b8b8f957d
Emma <Emma@Emmas>
2018-01-05 11:31:51 +0100

    controller, constructor i order

--- is3/src/is3/Controller.java
+++ is3/src/is3/Controller.java
@@ -19,9 +19,9 @@ public class Controller {
         Kund newCustomer = new Kund (cNumber, cName, adress);
             this.customers.addKund(newCustomer);
 	}
-	public void addCustomerOrder (String orderID, String delDate, String dDate, Kund customer, String cNumber) {
+	public void addCustomerOrder (String orderId, String delDate, String dDate, Kund customer, String cNumber) {
 		Kund newCustomer = customers.findKund(cNumber);
-		Order order = new Order (orderID, delDate, dDate, customer);
+		Order order = new Order (orderId, delDate, dDate, customer);
 		newCustomer.addOrder(order);
 		order.setCustomer(newCustomer);
 		customers.addKund(newCustomer);
@@ -29,7 +29,19 @@ public class Controller {
 	public void removeCustomer(String cNumberRemove) {
 	 customers.removeKund(cNumberRemove);
 	}
-	public void addOrderlist
-	
-	
+	public void addOrder(String OrderId, String delDate, String dDate) {
+		Kund newKund = customers.findKund(cNumber);
+		Order newOrder = new Order(OrderId);
+		newKund.addOrder(anOrder);
+		newOrder.setCustomer(newKund);
+	}
+	public void addOrderrad(String number, String amount) {
+		Order newOrder = 
+		Order newOrder = ordRowList.findOrder(OrderID);
+		
+		
+		
+		
+	}
+		
 }
