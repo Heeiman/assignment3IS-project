8b7df162246fb29809314f96a6b2ef59ebeb97f6
Anton <anton.hejman@gmail.com>
2018-01-09 15:06:27 +0100

    15:06

--- is3/src/is3/Test.java
+++ is3/src/is3/Test.java
@@ -28,14 +28,14 @@ public class Test {
 		
 		kr.removeCustomer("1");
 		
-		System.out.println(kr.findCustomer("1").getName() + kr.findCustomer("1").getcAdress() + kr.findCustomer("1").getcNumber());
+		//System.out.println(kr.findCustomer("1").getName() + kr.findCustomer("1").getcAdress() + kr.findCustomer("1").getcNumber());
 		
 		Product p1 = new Product("Tåg", "Modelltåg", "500");
 		Item item = new Item("12345");
 		OrderRow row = new OrderRow("1","50");
 		p1.addItem(item);
 		p1.addOrdRowP(row);
-		System.out.println(p1.getName() + p1.getCategory() + p1.getPrice() + p1.getItemList().values() + p1.getOrderRowP());
+		System.out.println(p1.getName() + p1.getCategory() + p1.getPrice());
 
 		
 		
