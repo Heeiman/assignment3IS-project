ef5aad931d48b91f91af9bf6dc4ef1952a0c5558
Erik <Erik@Minerva>
2018-01-10 15:39:01 +0100

    123

--- is3/src/is3/App.java
+++ is3/src/is3/App.java
@@ -170,7 +170,7 @@ public class App {
 				if (textField_Cnumber.getText().length() == 4) {	
 				try {	
 				cName = cName.substring(0,1).toUpperCase() + cName.substring(1).toLowerCase();
-				cAdress = cAdress.substring(0,1).toUpperCase() + cAdress.substring(1).toLowerCase();
+				cAdress = cAdress.substring(0,1).toUpperCase() + cAdress.substring(1).toLowerCase(); //BLA
 				controller.addCustomer(cNumber, cName, cAdress);
 				textPane.setText(cNumber + " " + cName + " " + cAdress);
 				textField_Cnumber.setText("");
