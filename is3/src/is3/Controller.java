a0d49fb2784c10bbcf9a3bf37ab8f457d8c00d43
Anton <anton.hejman@gmail.com>
2018-01-10 15:09:43 +0100

    15:09

--- is3/src/is3/Controller.java
+++ is3/src/is3/Controller.java
@@ -19,11 +19,9 @@ public class Controller {
 	}
 
 	public void addCustomer(String cNumber, String cName, String cAdress) {
-		Customer tmpCustomer = customers.findCustomer(cNumber);
-		if (tmpCustomer == null) {
 		Customer newCustomer = new Customer(cNumber, cName, cAdress);
 		this.customers.addCustomer(newCustomer);
-		}
+
 	}
 
 	public void addCustomerOrder(String orderID, String delDate, String cNumber) {
